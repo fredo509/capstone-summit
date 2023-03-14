3df859636ae54042146a12407c5a222c78907258
Fredo St Fleur <fredostfleur23@gmail.com>
2023-03-14 15:27:42 -0400

    -FIx Design -Update feature of object -Loop object and get content of -Change greeting image in text

--- script.js
+++ script.js
@@ -16,50 +16,37 @@ presentationSection.addEventListener('click', () => {
 });
 
 const speakers = [{
-  name: ['Fredo st fleur', 'Jean Marie', 'Jean Pierre', 'Marie claude'],
-  infoproject: ['Back end Dev', '2015'],
-  image: './images/project_1.svg',
-  projectDescription: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
-  proTech: ['html', 'javascript', 'css'],
-  liveUrl: 'https://github.com/fredo509/Portfolio_setup_mobile_first',
-  repoUrl: 'https://github.com/fredo509/Portfolio_setup_mobile_first',
+  name: 'Yochai Benkler',
+  image: './images/speaker_01 1.svg',
+  function: 'Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School',
+  profileDescription: 'Benkler studies commons-based peer production, and published his seminal book, The Wealth of Networks in 2006',
+},
+
+{
+  name: 'Mariane Jean',
+  image: './images/speaker_01 2.svg',
+  function: 'Mariane Professor of Entrepreneurial Legal Studies at Harvard Law School',
+  profileDescription: 'Mariane studies commons-based peer production, and published his seminal book, The Wealth of Networks in 2006',
 }];
 
-const bodyLoad = () => {
-  const dataContainer = document.createElement('section');
-  dataContainer.classList.add('speakers');
-  document.body.appendChild(dataContainer);
+const profile = document.getElementById('speakers');
 
-  dataContainer.innerHTML = `
-            <div class ="profile-card">
-                <div class="main-program ft-speakers">
-                    <p>Featured speakers </p>
-                    <div class="t-buttom"></div>
-                </div>
-                <div class="profile">
-                    <div class="profile-picture">
-                        <img src="./images/speaker_01 1.svg" alt="">
-                    </div>
-                    <div profile-about>
-                        <p class="profile-nom">Yochai Benkler</p>
-                        <p class="profile-fuction">Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School</p>
-                        <div class="t-buttom t-profile"></div>
-                        <p class="profile-d">Benkler studies commons-based peer production, and published his seminal book, The Wealth of Networks in 2006</p>
-                    </div>
-                </div>
-                <div class="profile">
-                    <div class="profile-picture">
-                        <img src="./images/speaker_01 2.svg" alt="">
-                    </div>
-                    <div profile-about>
-                        <p class="profile-nom">SohYeong Noh</p>
-                        <p class="profile-fuction">Director of Art Centre Nabi and a board member of CC Korea</p>
-                        <div class="t-buttom t-profile"></div>
-                        <p class="profile-d">As the main venue for new media art production in Korea, Nabi promotes cross-disciplinary collaboration and
-                        understanding among science technology, humanities, and the arts.</p>
-                    </div>
-                </div>
-            </div>
-            <button class="showMore">More</button>
-        `;
+let html = '';
+
+const bodyLoad = () => {
+  speakers.forEach((element, index) => {
+    html += `
+<div id="profile">
+  <div class="profile-picture">
+  <img src="${speakers[index].image}" alt="speaker image">
+</div>
+<div profile-about>
+<p class="profile-nom">${speakers[index].name}</p>
+<p class="profile-function">${speakers[index].function}</p>
+<div class="t-buttom t-profile"></div>
+<p class="profile-d">${speakers[index].profileDescription}</p>
+</div>
+</div>`;
+    profile.innerHTML = html;
+  });
 };
